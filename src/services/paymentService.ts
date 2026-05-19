92609a595c16f9ed55f021c7187fe8ce8d2fcb8e
Muhammad Umar <mumar@dax.local>
2026-05-19 20:37:38 +0500

    Forwarding

--- src/services/paymentService.ts
+++ src/services/paymentService.ts
@@ -13,7 +13,7 @@ if (!stripeSecretKey) {
 
 const stripe = stripeSecretKey
   ? new Stripe(stripeSecretKey, {
-      apiVersion: '2026-02-25.clover',
+      apiVersion: '2025-12-15.clover' as any,
     })  
   : (null as unknown as Stripe);
 
